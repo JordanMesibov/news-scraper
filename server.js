eae03c816eb55ed7dc7915fe0258ef42bd5e714b
shinyscyther <mesibovj@gmail.com>
2019-02-28 16:03:57 -0500

    saving work setting up server.js

--- server.js
+++ server.js
@@ -3,4 +3,24 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const axios = require("axios");
+const cheerio = require("cheerio");
 
+const db = require("./models");
+let PORT = 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect("mogodb://localhost/news", { useNewUrlParser: true });
+
+//set up the routes
+app.get("/scrape", function (req, res) {
+  axios.get("http://")
+})
