d64ce28d0ee7a2632a4eda5374632c37ce92a720
shinyscyther <mesibovj@gmail.com>
2019-02-28 16:27:47 -0500

    finished the server.js page using the in class mongoose scraper files as reference

--- server.js
+++ server.js
@@ -22,5 +22,59 @@ mongoose.connect("mogodb://localhost/news", { useNewUrlParser: true });
 
 //set up the routes
 app.get("/scrape", function (req, res) {
-  axios.get("http://")
-})
+  axios.get("https://www.bbc.com/").then(function(response) {
+    var $ = cheerio.load(response.data);
+    var articleArr = [];
+
+    $("div.media__content").each(function (i, element) {
+      //save result as an empty object that I can iterate over the responses and push to the object
+      var result = {};
+
+      result.title = $(this).children("a").text();
+      result.link = $(this).children("a").attr("href");
+
+      articleArr.push(result);
+
+    });
+
+    db.Article.create(articleArr).then(() => res.send("Just performed the scrape!")).catch(err => {
+      console.error(err);
+      res.json(err);
+    });
+
+  });
+});
+
+// in the news db I made, GET from the headlines
+app.get("/headlines", function (req, res) {
+  db.headlines.find({}).then(function (dbArticle) {
+    res.json(dbArticle);
+  }).catch(err => {
+    res.json(err);
+  });
+});
+
+// in the news db I made, GET the headlines/articles by their id parameter using express (app)
+app.get("/headlines/:id", function (req, res) {
+  db.headlines.findOne({ _id: req.params.id }).populate("note").then(function (dbArticle) {
+    res.json(dbArticle);
+  }).catch(function(err) {
+    res.json(err);
+  });
+});
+
+// in the news db I made, set up route for changing any articles' comment(s)
+app.post("/headlines/:id", function (req, res) {
+  db.Note.create(req.body).then(function (dbNote) {
+    return db.Article.findOneAndUpdate( {_id: req.params.id }, { new: true });
+  }).then(function(dbArticle) {
+    res.json(dbArticle);
+  }).catch(function(err) {
+    res.json(err);
+  });
+});
+
+// tell the server to start running
+app.listen(PORT, function() {
+  console.log("App is running on PORT " + PORT);
+});
